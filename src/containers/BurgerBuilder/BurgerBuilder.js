f37cee40a71d46b6863cb4e05d2a568f2aaf6131
mmimonir <monirdhk.it@gmail.com>
2020-05-01 00:20:39 +0600

    Create Dynamic Form

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,26 +95,22 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false })
   }
   purchageContinueHandler = () => {
-    this.setState({ loading: true })
-    //alert('You continue!')
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Md Moniru Islam',
-        address: {
-          street: 'Ka 26',
-          zipCode: '1219',
-          country: 'Bangladesh'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
+    // this.setState({ loading: true })
+
+    const queryParams = []
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i])
+      )
     }
-    axios
-      .post('/orders.json', order)
-      .then((res) => this.setState({ loading: false, purchasing: false }))
-      .catch((er) => this.setState({ loading: false, purchasing: false }))
+    queryParams.push('price=' + this.state.totalPrice)
+    const queryString = queryParams.join('&')
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    })
   }
   render() {
     const disabledInfo = {
